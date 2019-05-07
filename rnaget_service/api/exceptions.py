1fdc6170d130a1872fc597915c133c21dcb211e4
Adam Lipski <alipski@live.com>
2019-05-07 11:57:16 -0700

    Added scripts to manage RESTful db updates

--- rnaget_service/api/exceptions.py
+++ rnaget_service/api/exceptions.py
@@ -21,3 +21,12 @@ class IdentifierFormatError(ValueError):
     def __init__(self, identifier):
         message = "{} parameters must be correctly formatted UUID strings".format(identifier)
         super().__init__(message)
+
+
+class AuthorizationError(Exception):
+    """
+    Custom exception for failed authorization
+    """
+    def __init__(self):
+        message = "Key not authorized to perform this action"
+        super().__init__(message)
